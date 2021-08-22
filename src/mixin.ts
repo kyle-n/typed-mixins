d54672b4554c3eb723ca8e038b0c1c2a6773bae3
kyle-n <kylebnazario@gmail.com>
2021-08-22 17:03:06 -0400

    Returns class that creates new objects with the mixins

--- src/mixin.ts
+++ src/mixin.ts
@@ -14,20 +14,20 @@ const jumper: Jumpable = {
 }
 
 // https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type
-type UnionToIntersection<U> = 
-  (U extends any ? (k: U)=>void : never) extends ((k: infer I)=>void) ? I : never
-
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
 type MixinIntersection<T extends Array<any>> = UnionToIntersection<(T)[number]>
-
 type KeyedObject = {[key: string]: any};
 type Prototyped<T> = {prototype: T}
+type ClassType<T extends KeyedObject> = T extends Prototyped<KeyedObject> ? T : T['prototype'];
 type RealType<T extends KeyedObject> = T extends Prototyped<KeyedObject> ? T['prototype'] : T;
 
 function mixin<X extends RealType<KeyedObject>, Y extends RealType<KeyedObject>>(base: X, mixins: Array<Y>) {
   const x = 1;
-  return x as RealType<X> & MixinIntersection<Array<RealType<Y>>>
+  // @ts-ignore
+  return x as (new () => RealType<X> & MixinIntersection<Array<RealType<Y>>>)
 }
 
-const x = mixin(Person, [Jumpable, Flyable])
 const y = {1: 'na'}
-const z = mixin(Person, [y])
+const XClass = mixin(Person, [Jumpable, Flyable])
+const v = new XClass()
+
