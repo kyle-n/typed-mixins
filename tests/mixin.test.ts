878afea6ab88b1c1a60d2132c0a7397737690c26
kyle-n <kylebnazario@gmail.com>
2021-08-24 22:16:40 -0400

    Tests mixin()

---
+++ tests/mixin.test.ts
@@ -0,0 +1,44 @@
+import { mixin } from '../src/mixin';
+
+describe('mixin', () => {
+  class Person {
+    name!: string;
+  }
+  class Jumpable {
+    static legs = 2;
+    jump() {}
+    doubleJump = () => {};
+  }
+  class Flyable {
+    static soar() {}
+    height = 12;
+  }
+
+  const SuperHero = mixin(Person, [Jumpable, Flyable]);
+  let hero: Person & Jumpable & Flyable;
+
+  beforeEach(() => {
+    hero = new SuperHero();
+  });
+
+  it('should copy instance properties', () => {
+    hero.jump();
+    hero.doubleJump();
+    expect(hero.height).toBe(12);
+  });
+
+  it('should copy static properties', () => {
+    SuperHero.soar();
+    expect(SuperHero.legs).toBe(2);
+  });
+
+  it('should be extendable', () => {
+    class CapedSuperHero extends SuperHero.extendable {
+      twirl() {
+        this.doubleJump();
+      }
+    }
+    const capeGuy = new CapedSuperHero();
+    capeGuy.twirl();
+  });
+});
